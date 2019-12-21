9059db815c517d82f1f63dbde3b4a4921e0d46ea
olivermanzi <olivermanzi.dev@gmail.com>
2019-12-21 23:47:00 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <b-jumbotron>Coming soon... 🤠</b-jumbotron>
+    <b-container>
+      <b-row>
+        <b-col>
+          <router-view />
+        </b-col>
+      </b-row>
+      <b-row id="footer">
+        <b-col>Oliver Manzi &hearts;</b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+};
+</script>
+
+<style>
+#app {
+  text-align: center;
+}
+</style>
