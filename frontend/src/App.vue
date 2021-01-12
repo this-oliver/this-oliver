a6fb7aca7687c6e109de2222b48a6284ec8122a2
olivermanzi <olivermanzi.dev@gmail.com>
2021-01-12 22:20:51 +0100

    [wip] navbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,28 @@
 <template>
 	<div :id="theme" class="app">
+		<b-navbar :variant="bootstrapTheme" :type="bootstrapTheme">
+			<b-navbar-brand>
+				Oliver Manzi
+			</b-navbar-brand>
+
+			<b-navbar-toggle target="nav" />
+			<b-collapse id="nav" is-nav>
+				<b-navbar-nav align="end">
+					<b-nav-form>
+						<b-button @click="toggleTheme">
+							<span v-if="darkTheme">
+								🌞
+							</span>
+							<span v-else>
+								🌚
+							</span>
+						</b-button>
+					</b-nav-form>
+				</b-navbar-nav>
+			</b-collapse>
+		</b-navbar>
 		<HelloWorld msg="Weeelcome to Your Vue.js App" />
 		hey
-		<b-button @click="toggleTheme">
-			<span v-if="darkTheme">
-				🌞
-			</span>
-			<span v-else>
-				🌚
-			</span>
-		</b-button>
 	</div>
 </template>
 
@@ -30,10 +43,13 @@ export default {
 		theme: function() {
 			return this.darkTheme ? "dark-theme" : "light-theme";
 		},
+		bootstrapTheme: function() {
+			return this.darkTheme ? "dark" : "light";
+		},
 	},
 	methods: {
 		toggleTheme: function() {
-			this.darkTheme = !this.darktheme;
+			this.darkTheme = !this.darkTheme;
 		},
 	},
 };
