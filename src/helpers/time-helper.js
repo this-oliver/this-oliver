d52fd880ba4ffd862859b8300538fbb87ee66b96
olivermanzi <olivermanzi.dev@gmail.com>
2021-03-28 14:11:35 +0200

    reverses xp sort

--- src/helpers/time-helper.js
+++ src/helpers/time-helper.js
@@ -38,9 +38,11 @@ export let sortLatestArticles = articles =>{
 export let sortLatestXp = xpList => {
 
 	let compare = (a, b) => {
-		if (a < b) {
+		a = a.startYear;
+		b = b.startYear;
+		if (a > b) {
 			return -1;
-		} else if (a > b) {
+		} else if (a < b) {
 			return 1;
 		} else {
 			return 0;
