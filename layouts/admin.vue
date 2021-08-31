a0f16cd46a1ea84e63017d3c42fb21a79052d003
olivermanzi <olivermanzi.dev@gmail.com>
2021-08-31 11:53:25 +0200

    removes unnecessary index child routes

--- layouts/admin.vue
+++ layouts/admin.vue
@@ -20,9 +20,9 @@
 </template>
 
 <script>
-	import TheFooter from "../components/TheFooter.vue";
-	import TheNavbar from "../components/TheNavbar.vue";
+	import TheNavbar from "~/components/TheNavbar.vue";
 	import TheSidebar from "~/components/TheSidebar.vue";
+	import TheFooter from "~/components/TheFooter.vue";
 	import TheNotificationBar from "~/components/TheNotificationBar.vue";
 
 	export default {
