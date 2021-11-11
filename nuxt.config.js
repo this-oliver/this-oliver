dce990b5ab4be2fc69d5480689c45e5e92ed12bc
olivermanzi <olivermanzi.dev@gmail.com>
2021-11-11 16:47:30 +0100

    removes dark admin theme

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,11 +26,7 @@ export default {
 	},
 
 	// Global CSS: https://go.nuxtjs.dev/config-css
-	css: [
-		"~/assets/css/app.css",
-		"~/assets/css/fonts.css",
-		"~/assets/css/themes.css"
-	],
+	css: ["~/assets/css/app.css", "~/assets/css/fonts.css"],
 
 	// Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
 	plugins: [
