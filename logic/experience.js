87a3bef7fc46ab4ae093314222b1808b7ddfdce8
Oliver Manzi <olivermanzi.dev@gmail.com>
2022-01-23 12:48:39 +0100

    fixes sorting algorith for experiences

--- logic/experience.js
+++ logic/experience.js
@@ -4,7 +4,12 @@
  * @returns {Array}
  */
 export const sortLatestExperiences = function (experiences) {
-	const sortedByStartDate = experiences.sort(function (experienceA, experienceB) {
+	// sort experiences by start date: experiences that
+	// started earlier should be at the end of the array
+	const sortedByStartDate = experiences.sort(function (
+		experienceA,
+		experienceB
+	) {
 		/**
 		 * when comparing A and B:
 		 * - return 1 to sort B before A,
@@ -20,18 +25,44 @@ export const sortLatestExperiences = function (experiences) {
 		}
 	});
 
-	return sortedByStartDate.sort(function (experienceA, experienceB) {
+	// sort experiences by end date: experiences that
+	// ended earlier should be at the end of the array
+	const sortedByEndDate = sortedByStartDate.sort(function (
+		experienceA,
+		experienceB
+	) {
 		/**
 		 * when comparing A and B:
 		 * - return 1 to sort B before A,
 		 * - return -1 to sort A before B, and
 		 * - return 0 to sort A and B equally
 		 */
+
 		if (experienceA.endYear < experienceB.endYear) {
 			return 1;
 		} else if (experienceA.endYear > experienceB.endYear) {
 			return -1;
 		} else {
+			return 0;
+		}
+	});
+
+	// sort experiences by end date: experiences that
+	// have no end date (null) should be at the beginning of the array
+	return sortedByEndDate.sort(function (experienceA, experienceB) {
+		/**
+		 * when comparing A and B:
+		 * - return 1 to sort B before A,
+		 * - return -1 to sort A before B, and
+		 * - return 0 to sort A and B equally
+		 */
+
+		// check if any of the experiences have null (e.g. present)
+		if (experienceA.endYear === null && experienceB.endYear !== null) {
+			return -1;
+		} else if (experienceA.endYear !== null && experienceB.endYear === null) {
+			return 1;
+		} else if (experienceA.endYear === null && experienceB.endYear === null) {
 			return 0;
 		}
 	});
