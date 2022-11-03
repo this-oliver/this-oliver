6f6abd3ef779ccf10de62de122d0f8cd3f960833
Oliver Manzi <olivermanzi.dev@gmail.com>
2022-11-03 17:08:19 +0100

    minor frontend updates
    Squashed commit of the following:
    
    commit 0297695ad1cf7776feb397d213d413aec76149fe
    Author: Oliver Manzi <olivermanzi.dev@gmail.com>
    Date:   Thu Nov 3 17:07:53 2022 +0100
    
        [patch-f] adds valid store path for setting theme
    
    commit 8f5be4f45857635a71cf7cacfda2026118de7415
    Author: Oliver Manzi <olivermanzi.dev@gmail.com>
    Date:   Thu Nov 3 17:03:41 2022 +0100
    
        [feature-f] sets theme based on system preference
    
    commit c89fe2fd8fb579293452913435254f3504ea5280
    Author: Oliver Manzi <olivermanzi.dev@gmail.com>
    Date:   Wed Nov 2 14:19:07 2022 +0100
    
        [patch-f] fixes bug that showed admin nav buttons in sidebar
    
    commit 5b4d77374e47b1ab65fe7e3c40f00b857e8df2b4
    Author: Oliver Manzi <olivermanzi.dev@gmail.com>
    Date:   Tue Nov 1 14:27:08 2022 +0100
    
        [patch-f] updates favicons
    
    commit 10dd21771eedf5891811100d71be3635ee999078
    Author: Oliver Manzi <olivermanzi.dev@gmail.com>
    Date:   Tue Nov 1 12:24:27 2022 +0100
    
        [patch-f] updates build commands
    
    commit be02478126ff4eadf89dd5d2fccca9215e656b9b
    Author: Oliver Manzi <olivermanzi.dev@gmail.com>
    Date:   Tue Nov 1 12:09:53 2022 +0100
    
        [patch-f] sets node engine
    
    commit f8d4e65dba9acd1a97e2b4c2015979106f573c90
    Author: Oliver Manzi <olivermanzi.dev@gmail.com>
    Date:   Tue Nov 1 12:02:12 2022 +0100
    
        [patch-f] adds build flag

--- frontend/plugins/theme.client.js
+++ frontend/plugins/theme.client.js
@@ -4,6 +4,6 @@ export default function ({ store }) {
 		// check system preference
 		const darkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
 		// set system preference
-		store.commit("theme/setDarkMode", darkMode);
+		store.commit("app/theme/setDarkMode", darkMode);
 	}
 }
