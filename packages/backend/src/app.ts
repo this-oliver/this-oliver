8d28cabd43a78960a326f4643376a862e9973ff2
oliverrr <32515201+this-oliver@users.noreply.github.com>
2024-04-30 07:03:51 +0200

    Add containerization (#225)
    - adds docker files
    - adds k8 config files
    - moves frontend/backend into packages
    - updates build and deploy script

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -1,4 +1,4 @@
-import { NODE_ENV } from "./config/env";
+import { NODE_ENV, CORS_ORIGIN } from "./config/env";
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
@@ -7,10 +7,15 @@ import router from "./router";
 const app = express();
 
 app.use(express.json());
-app.use(cors());
 
 if (NODE_ENV == "development") {
 	app.use(morgan("dev"));
+}
+
+if (CORS_ORIGIN) {
+	app.use(cors({ origin: CORS_ORIGIN }));
+} else {
+	app.use(cors());
 }
 
 app.use(router);
