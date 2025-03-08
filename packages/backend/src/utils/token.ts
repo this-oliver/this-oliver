6d5afb611ff43bf54ca21fe5ec72640de866b465
Oliver Manzi <olivermanzi.dev@gmail.com>
2025-03-08 23:07:32 +0100

    removes unused token functions

--- packages/backend/src/utils/token.ts
+++ packages/backend/src/utils/token.ts
@@ -1,8 +1,6 @@
-import type { Request } from "express";
 import type { BaseError } from "../types/error";
 import Jwt from "jsonwebtoken";
 import { JWT_SECRET } from "../config/env";
-import * as UserData from "../data/users";
 
 function getToken(value: string): string {
   const payload = { data: value };
@@ -21,9 +19,7 @@ function verifyToken(token: string): boolean {
   return true;
 }
 
-function extractToken(req: Request): string {
-  const token = req.headers?.authorization?.split(" ")[1];
-
+function extractToken(token: string): any {
   if (!token) {
     throw { message: "invalid authentication credentials", status: 401 } as BaseError;
   }
@@ -37,21 +33,7 @@ function extractToken(req: Request): string {
   return (result.payload as any).data; // cast to any to avoid type error
 }
 
-async function authenticateRequest(req: Request) {
-  const token = extractToken(req);
-  let admin;
-
-  try {
-    admin = await UserData.getUser();
-  } catch {
-    throw { message: "invalid authentication credentials", status: 401 } as BaseError;
-  }
-
-  return admin._id === token;
-}
-
 export {
-  authenticateRequest,
   extractToken,
   getToken,
   verifyToken
