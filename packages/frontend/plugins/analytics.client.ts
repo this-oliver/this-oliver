08b1b99154b7b5b58542578b225c67b55a18ca00
Oliver Manzi <olivermanzi.dev@gmail.com>
2025-07-10 23:46:09 +0200

    fix(frontend): update simple analytics for ssr

--- packages/frontend/plugins/analytics.client.ts
+++ packages/frontend/plugins/analytics.client.ts
@@ -1,29 +1,12 @@
 import SimpleAnalytics from "simple-analytics-vue";
-import { useStorage } from "~/composables/useStorage";
-import { LOCAL_AUTH_ACCESS_TOKEN } from "~/stores/auth-store";
 
 export default defineNuxtPlugin((nuxtApp) => {
-  let skipAnalytics = false;
-
-  // token exists = logged in
-  const { get } = useStorage();
-  const token = get(LOCAL_AUTH_ACCESS_TOKEN);
-  const tokenExists = token && token.length > 0;
-
-  // if we are logged in, skip analytics
-  if (tokenExists) {
-    skipAnalytics = true;
-
-    // eslint-disable-next-line no-console
-    console.info("Skipping analytics because auth token exists");
-  }
+  let skipAnalytics: boolean = false;
 
   // skip analytics if we are not in production
   if (process.env.NODE_ENV !== "production") {
+    console.warn("Skipping analytics in non-production env");
     skipAnalytics = true;
-
-    // eslint-disable-next-line no-console
-    console.info("Skipping analytics in non-production env");
   }
 
   nuxtApp.vueApp.use(SimpleAnalytics, { skip: skipAnalytics });
