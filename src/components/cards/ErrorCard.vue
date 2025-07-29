f2c90902891b277955ed94dc9b60a86310a408b9
oliverrr <32515201+this-oliver@users.noreply.github.com>
2025-07-29 11:32:06 +0200

    fix(frontend): removes baselist (#259)
    * fix(frontend): removes baselist
    
    * fix(frontend): centralizes error messages with error card

---
+++ src/components/cards/ErrorCard.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+const props = defineProps({
+  message: {
+    type: String,
+    default: undefined
+  },
+  redirect: {
+    type: String,
+    default: "/"
+  }
+});
+</script>
+
+<template>
+  <div class="flex flex-col gap-2 items-center">
+    <img
+      src="https://media4.giphy.com/media/QMHoU66sBXqqLqYvGO/giphy.gif"
+      alt="Meme of a dog in a burning room saying 'This is fine'"
+      width="100%"
+      class="mt-2">
+
+    <slot>
+      <h2 class="text-lg font-semibold text-center">
+        {{ props.message || "An error occurred" }}
+      </h2>
+    </slot>
+
+    <slot name="actions">
+      <base-btn :to="props.redirect" class="bg-red-400">
+        <icon name="mdi-arrow-left" class="h-4 w-4 mr-1" />
+        Return Home
+      </base-btn>
+    </slot>
+  </div>
+</template>
--- src/pages/notes/index.vue
+++ src/pages/notes/index.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import type { ActionItem, Note } from "~/types";
-import { h } from "vue";
 import NoteCard from "~/components/cards/NoteCard.vue";
 import { useRouterQuery } from "~/composables/useRouterQuery";
 import { useNoteStore } from "~/stores/note-store";
@@ -22,103 +20,11 @@ useSeoMeta({
 const noteStore = useNoteStore();
 const query = useRouterQuery();
 
-const { data, status } = useAsyncData("notes", async () => {
-  // fetch notes from store
-  return await noteStore.indexNotes();
+const { status } = await useAsyncData("notes", () => {
+  return noteStore.indexNotes();
 });
 
-const showFilter = ref(false);
-
-const isLoading = computed<boolean>(() => {
-  return status.value === "pending";
-});
-
-const showReset = computed<boolean>(() => {
-  return noteStore.filter.tags.length > 0;
-});
-
-const getNotes = computed<Note[]>(() => {
-  let filteredNotes: Note[] = data.value || [];
-
-  // filter query
-  if (noteStore.filter.query.length > 0) {
-    filteredNotes = filteredNotes.filter((note) => {
-      return note.title.toLowerCase().includes(noteStore.filter.query.toLowerCase());
-    });
-  }
-
-  // filter tags
-  if (noteStore.filter.tags.length > 0) {
-    filteredNotes = filteredNotes.filter((note) => {
-      return note.tags.some(tag => tagInFilter(tag));
-    });
-  }
-
-  return filteredNotes;
-});
-
-interface Tag {
-  name: string
-  filtered: boolean
-};
-const getTags = computed<Tag[]>(() => {
-  return noteStore.tags.flat()
-    .map((tag) => {
-      return {
-        name: tag,
-        filtered: tagInFilter(tag)
-      };
-    })
-    .sort((a, b) => {
-      if (a.filtered && !b.filtered) {
-        return -1;
-      }
-      if (!a.filtered && b.filtered) {
-        return 1;
-      }
-      return 0;
-    });
-});
-
-const options = computed<ActionItem[]>(() => {
-  const hasSomethingToFilter = getTags.value.length > 0;
-
-  return hasSomethingToFilter
-    ? [
-      {
-        label: "Filter",
-        color: "primary",
-        icon: "mdi-filter",
-        action: () => {
-          showFilter.value = !showFilter.value;
-        }
-      }
-    ]
-    : [];
-});
-
-const components = computed(() => {
-  return getNotes.value.map((note) => {
-    // return a NoteCard component with the note prop set to the note
-    return h(NoteCard, { note });
-  });
-});
-
-function tagInFilter(tag: string): boolean {
-  return noteStore.filter.tags.includes(tag);
-}
-
-function handleSelectedTagEvent(tag: Tag, event: MouseEvent): void {
-  if (!event?.target)
-    return;
-
-  const input = event.target as HTMLInputElement;
-  if (input.checked) {
-    noteStore.addTagToFilter(tag.name);
-  } else {
-    noteStore.removeTagFromFilter(tag.name);
-  }
-}
+const showSearchField = ref<boolean>(false);
 
 // deep watch filters and update route query
 watch(
@@ -132,80 +38,51 @@ watch(
   }
 );
 
-watch(
-  () => noteStore.filter.tags,
-  async (newTags) => {
-    if (newTags.length > 0) {
-      await query.add("tags", newTags.join(","));
-    } else {
-      await query.remove("tags");
-    }
-  },
-  { deep: true }
-);
-
 onMounted(async () => {
-  // set filters from query params
   if (query.has("q")) {
     noteStore.filter.query = query.get("q") as string;
-  }
-
-  if (query.has("tags")) {
-    noteStore.filter.tags = query.get("tags").split(",");
   }
 });
 </script>
 
 <template>
-  <base-page title="Notes" class="flex flex-col gap-4 md:w-6/12 md:mx-auto">
-    <div class="w-full flex flex-col">
-      <base-list
-        v-if="!isLoading"
-        label="notes"
-        allow-search
-        :options="options"
-        :is-loading="isLoading"
-        :components="components"
-        :search="noteStore.filter.query"
-        @search="(q: string) => noteStore.filter.query = q" />
-    </div>
-
-    <base-sidebar
-      id="notes-sidebar"
-      :show="showFilter"
-      label="Filter Notes"
-      class="bg-stone-600 p-2 right-0 w-80"
-      @close="showFilter = false">
-      <div class="flex flex-col gap-4">
-        <div v-if="getTags.length > 0" id="tags" class="flex flex-col gap-2">
-          <h3>
-            Tags
-          </h3>
-
-          <div v-for="tag in getTags" :key="tag.name" class="flex items-center gap-2">
-            <input
-              v-model="tag.filtered"
-              type="checkbox"
-              class="form-checkbox h-4 w-4 text-green-600 border-gray-300 rounded focus:ring-green-500"
-              @change="handleSelectedTagEvent(tag, event)">
-            <v-checkbox-btn
-              v-model="tag.filtered"
-              color="success"
-              @update:model-value="(value: boolean) => value ? noteStore.addTagToFilter(tag.name) : noteStore.removeTagFromFilter(tag.name)" />
-
-            <span>
-              {{ tag.name }}
-            </span>
-          </div>
+  <base-page title="Notes">
+    <div class="md:w-6/12 md:mx-auto flex flex-col gap-2">
+      <div id="filter" class="h-10 mb-2 flex md:grid md:grid-cols-2 gap-2">
+        <div v-if="showSearchField" class="p-1 flex gap-2 items-center brutalist-outline">
+          <input v-model="noteStore.filter.query" placeholder="Search..." class="w-full h-full">
+          <button class="p-2 flex items-center cursor-pointer" @click="noteStore.filter.query = ''; showSearchField = false;">
+            <icon name="mdi-close" class="text-lg" />
+          </button>
         </div>
 
         <base-btn
-          v-if="showReset"
-          class="bg-red-400 mt-auto"
-          @click="noteStore.resetFilter()">
-          Reset Filters
+          v-else
+          class="w-5/12 flex items-center gap-2"
+          @click="showSearchField = true">
+          <icon name="mdi-magnify" class="h-4 w-4 mr-1" />
+          Search
         </base-btn>
       </div>
-    </base-sidebar>
+
+      <div v-if="status === 'pending'">
+        Fetching notes...
+      </div>
+
+      <div v-else-if="status === 'error'">
+        Failed to fetch notes.
+      </div>
+
+      <div v-else-if="noteStore.getFilteredNotes.length === 0">
+        No notes found.
+      </div>
+
+      <div v-else class="flex flex-col gap-4">
+        <NoteCard
+          v-for="note in noteStore.getFilteredNotes"
+          :key="note._id"
+          :note="note" />
+      </div>
+    </div>
   </base-page>
 </template>
