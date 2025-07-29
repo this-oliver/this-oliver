75055f306c5a1e2f68f2dc99729e9b2e105d4a94
oliverrr <32515201+this-oliver@users.noreply.github.com>
2025-07-29 11:26:20 +0200

    fix: analytic plugin failing (#260)

--- src/plugins/analytics.client.ts
+++ src/plugins/analytics.client.ts
@@ -1,9 +1,8 @@
-import process from "node:process";
 import SimpleAnalytics from "simple-analytics-vue";
 
 export default defineNuxtPlugin((nuxtApp) => {
   // only run analytics in production
-  if (process.env.NODE_ENV === "production") {
+  if (import.meta.dev === false) {
     nuxtApp.vueApp.use(SimpleAnalytics);
   } else {
     console.warn("Skipping analytics in non-production env");
