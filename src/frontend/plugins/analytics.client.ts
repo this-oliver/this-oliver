03edefbc286055e4c220420722f4683327544119
Oliver Manzi <olivermanzi.dev@gmail.com>
2025-08-11 17:46:05 +0200

    fix(frontend): import.meta crashing in production

--- src/frontend/plugins/analytics.client.ts
+++ src/frontend/plugins/analytics.client.ts
@@ -1,10 +1,7 @@
 import SimpleAnalytics from "simple-analytics-vue";
 
 export default defineNuxtPlugin((nuxtApp) => {
-  // only run analytics in production
-  if (import.meta.dev === false) {
-    nuxtApp.vueApp.use(SimpleAnalytics);
-  } else {
-    console.warn("Skipping analytics in non-production env");
-  }
+  nuxtApp.vueApp.use(SimpleAnalytics, {
+    skip: process.env.NODE_ENV !== "production"
+  });
 });
