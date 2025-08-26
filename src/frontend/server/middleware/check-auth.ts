8cf52aba1c333885f705c4a20b0aae5465c4a84b
Oliver Manzi <olivermanzi.dev@gmail.com>
2025-08-26 15:28:32 +0200

    fix(frontend): set middleware errors to fatal

--- src/frontend/server/middleware/check-auth.ts
+++ src/frontend/server/middleware/check-auth.ts
@@ -3,15 +3,17 @@ export default defineEventHandler((event) => {
 
   if (!cmsApiToken || cmsApiToken.trim().length === 0) {
     throw createError({
-      statusCode: 500,
-      statusMessage: "NUXT_CMS_API_TOKEN must be set in the runtime config."
+      statusCode: 503,
+      statusMessage: "NUXT_CMS_API_TOKEN must be set in the runtime config.",
+      fatal: true
     });
   }
 
   if (!cmsApiUrl || cmsApiUrl.trim().length === 0) {
     throw createError({
-      statusCode: 500,
-      statusMessage: "NUXT_CMS_API_URL must be set in the runtime config."
+      statusCode: 503,
+      statusMessage: "NUXT_CMS_API_URL must be set in the runtime config.",
+      fatal: true
     });
   }
 });
