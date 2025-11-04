914660f293a1340031ea07bed833b6fc7071bef1
Oliver Manzi <olivermanzi.dev@gmail.com>
2025-11-04 20:10:57 +0100

    fix(frontend): seos not working for notes

--- src/frontend/pages/notes/[slug].vue
+++ src/frontend/pages/notes/[slug].vue
@@ -14,25 +14,27 @@ const { data, status } = await useAsyncData("note", async () => {
   const slug = router.currentRoute.value.params.slug as string;
   const note: Note = await $fetch(`/api/notes/${slug}`);
 
-  const title = `${note.title} - oliverrr`;
-  const description = note.content.substring(0, 150) + (note.content.length > 150 ? "..." : "");
+  return note;
+});
+
+const note = computed<Note | null>(() => data.value || null);
+const noteDate = computed<string>(() => note.value ? formatDate(note.value?.createdAt) : "");
+
+if (note.value) {
+  const title = `${note.value.title} - oliverrr`;
+  const description = note.value.content.substring(0, 150) + (note.value.content.length > 150 ? "..." : "");
 
   useSeoMeta({
     title,
     description,
     author: "oliverrr",
     ogType: "article",
-    ogUrl: `https://www.oliverrr.net/notes/${note.slug}`,
-    ogTitle: note.title,
-    ogDescription: note.content,
+    ogUrl: `https://www.oliverrr.net/notes/${note.value.slug}`,
+    ogTitle: note.value.title,
+    ogDescription: note.value.content,
     ogSiteName: "oliverrr's notes"
   });
-
-  return note;
-});
-
-const note = computed<Note | null>(() => data.value || null);
-const noteDate = computed<string>(() => note.value ? formatDate(note.value?.createdAt) : "");
+}
 </script>
 
 <template>
